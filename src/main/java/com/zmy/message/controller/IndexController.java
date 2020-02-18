bf05a2aafb19188a94e31a8ba42e66f0831cf75b
zmy <1332387681@qq.com>
2020-02-18 08:37:43 +0800

    login in github

--- src/main/java/com/zmy/message/controller/IndexController.java
+++ src/main/java/com/zmy/message/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.zmy.message.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-    public void Index(){
-
+    @GetMapping("index")
+    public String index(@RequestParam()){
+        return index;
     }
 
 }
