8e2ef5b4ef1e59986c46b2b030ed11cb5a7df563
zmy <1332387681@qq.com>
2020-02-19 13:39:38 +0800

    配置文件配置参数

--- src/main/java/com/zmy/message/controller/loginController.java
+++ src/main/java/com/zmy/message/controller/loginController.java
@@ -4,6 +4,7 @@ import com.zmy.message.dto.AccessTokenDTO;
 import com.zmy.message.dto.GithubUser;
 import com.zmy.message.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class loginController {
     @Autowired
     GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientid;
+    @Value("${github.client.sercet}")
+    private String clientsercet;
+    @Value("${github.redirect.uri}")
+    private String redirecturi;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("267f9a0cfe7cc0e3c4e6");
-        accessTokenDTO.setClient_secret("5d49f30af5b6c09cfdfa9a7859310cf0d90aaa74");
+        accessTokenDTO.setClient_id(clientid);
+        accessTokenDTO.setClient_secret(clientsercet);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirecturi);
         accessTokenDTO.setState(state);
         String asseccToken = githubProvider.getAsseccToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(asseccToken);
-        System.out.println(user);
+        System.out.println(user.getName());
         return "index";
     }
 
